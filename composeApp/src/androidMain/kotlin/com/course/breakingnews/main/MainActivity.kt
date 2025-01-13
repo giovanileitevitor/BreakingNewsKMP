649d201412ef7c4f6b1d6ad102a25763c2ac16a0
lucas.esteves <lucas.coelho9280@gmail.com>
2025-01-13 17:27:42 -0300

    Implementing DS Components

---
+++ composeApp/src/androidMain/kotlin/com/course/breakingnews/main/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.course.breakingnews.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.course.breakingnews.ui.card.BreakingNewsCard
+import com.course.breakingnews.ui.content.DetailsContentSection
+import com.course.breakingnews.ui.header.DetailsHeaderSection
+import com.course.breakingnews.ui.top.BreakingNewsTopBar
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT,
+            )
+        )
+        setContent {
+            App()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComponentsPreview() {
+    Column {
+        BreakingNewsTopBar(onClick = {})
+        BreakingNewsCard(
+            title = "5 things to know about the 'conundrum' of lupus",
+            author = "Matt Villano",
+            date = "Sunday, 9 May 2021",
+            onClick = {}
+        )
+        DetailsHeaderSection(onClick = {})
+        DetailsContentSection(
+            content = """
+                    LONDON — Cryptocurrencies “have no intrinsic value” and people who invest in them should be prepared to lose all their money, Bank of England Governor Andrew Bailey said.
+
+                    Digital currencies like bitcoin, ether and even dogecoin have been on a tear this year, reminding some investors of the 2017 crypto bubble in which bitcoin blasted toward $20,000, only to sink as low as $3,122 a year later.
+
+                    Asked at a press conference Thursday about the rising value of cryptocurrencies, Bailey said: “They have no intrinsic value. That doesn’t mean to say people don’t put value on them, because they can have extrinsic value. But they have no intrinsic value.”
+                """.trimIndent()
+        )
+    }
+}
+
+@Preview
+@Composable
+fun AppAndroidPreview() {
+    App()
+}
---
+++ composeApp/src/commonMain/kotlin/com/course/breakingnews/features/details/DetailsScreen.kt
@@ -0,0 +1,51 @@
+package com.course.breakingnews.features.details
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.course.breakingnews.ui.content.DetailsContentSection
+import com.course.breakingnews.ui.header.DetailsHeaderSection
+
+@Composable
+fun DetailsScreen(
+    onBackPressed: () -> Unit
+) {
+    DetailsContent(onBackPressed = onBackPressed)
+}
+
+@Composable
+fun DetailsContent(
+    onBackPressed: () -> Unit
+) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = Color.White,
+        topBar = {},
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                DetailsHeaderSection(onClick = { onBackPressed.invoke() })
+
+                DetailsContentSection(
+                    content = """
+                    LONDON — Cryptocurrencies “have no intrinsic value” and people who invest in them should be prepared to lose all their money, Bank of England Governor Andrew Bailey said.
+
+                    Digital currencies like bitcoin, ether and even dogecoin have been on a tear this year, reminding some investors of the 2017 crypto bubble in which bitcoin blasted toward $20,000, only to sink as low as $3,122 a year later.
+
+                    Asked at a press conference Thursday about the rising value of cryptocurrencies, Bailey said: “They have no intrinsic value. That doesn’t mean to say people don’t put value on them, because they can have extrinsic value. But they have no intrinsic value.”
+                """.trimIndent()
+                )
+            }
+        }
+    )
+}
